41064dbdd4fbddb92b47268634896a5ed6d0195c
Jorropo <jorropo.pgm@gmail.com>
2020-12-16 20:25:41 +0100

    feat: add support for using a custom DialFunc
    This aims to make go-dns compatible with proxy usage.
    
    Signed-off-by: Jorropo <jorropo.pgm@gmail.com>

--- dot.go
+++ dot.go
@@ -54,6 +54,12 @@ func NewDoTResolver(server string, options ...DoTOption) (*net.Resolver, error) 
 		opts.config.ServerName = server
 	}
 
+	// setup the dialFunc
+	if opts.dialFunc == nil {
+		var d net.Dialer
+		opts.dialFunc = d.DialContext
+	}
+
 	// create the resolver
 	var resolver = net.Resolver{
 		PreferGo:     true,
@@ -63,9 +69,8 @@ func NewDoTResolver(server string, options ...DoTOption) (*net.Resolver, error) 
 	// setup dialer
 	var index uint32
 	resolver.Dial = func(ctx context.Context, network, address string) (net.Conn, error) {
-		var d net.Dialer
 		s := atomic.LoadUint32(&index)
-		conn, err := d.DialContext(ctx, "tcp", opts.addrs[s])
+		conn, err := opts.dialFunc(ctx, "tcp", opts.addrs[s])
 		if err != nil {
 			atomic.CompareAndSwapUint32(&index, s, (s+1)%uint32(len(opts.addrs)))
 			return nil, err
@@ -91,17 +96,20 @@ type dotOpts struct {
 	addrs     []string
 	cache     bool
 	cacheOpts []CacheOption
+	dialFunc  DialFunc
 }
 
 type (
 	dotConfig    tls.Config
 	dotAddresses []string
 	dotCache     []CacheOption
+	dotDialFunc  DialFunc
 )
 
 func (o *dotConfig) apply(t *dotOpts)   { t.config = (*tls.Config)(o) }
 func (o dotAddresses) apply(t *dotOpts) { t.addrs = ([]string)(o) }
 func (o dotCache) apply(t *dotOpts)     { t.cache = true; t.cacheOpts = ([]CacheOption)(o) }
+func (o dotDialFunc) apply(t *dotOpts)  { t.dialFunc = (DialFunc)(o) }
 
 // DoTConfig sets the tls.Config used by the resolver.
 func DoTConfig(config *tls.Config) DoTOption { return (*dotConfig)(config) }
@@ -113,3 +121,7 @@ func DoTAddresses(addresses ...string) DoTOption { return dotAddresses(addresses
 
 // DoTCache adds caching to the resolver, with the given options.
 func DoTCache(options ...CacheOption) DoTOption { return dotCache(options) }
+
+// DoTDialFunc sets the DialFunc used by the resolver.
+// By default net#Dialer.DialContext is used.
+func DoTDialFunc(f DialFunc) DoTOption { return dotDialFunc(f) }
